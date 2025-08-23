246a3f70cb332553949956a822fad6aa657f6618
saurabh <saurabhyadav42399@gmail.com>
2025-08-23 19:58:06 +0530

    override fn on EE

--- src/ExecutionEngine.sol
+++ src/ExecutionEngine.sol
@@ -1,6 +1,19 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.22;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
 
-// contract ExecutionEngine{
+import {DexHelper} from "./abstract/DexHelper.sol";
+import {Admin2Step} from "./abstract/Admin2Step.sol";
+import {UUPSUpgradeable} from "@solady/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@solady/utils/Initializable.sol";
 
-// }
+contract ExecutionEngine is DexHelper, Admin2Step, UUPSUpgradeable, Initializable {
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address owner) public initializer {
+        _setAdmin(owner);
+    }
+
+    function _authorizeUpgrade(address) internal override onlyAdmin {}
+}

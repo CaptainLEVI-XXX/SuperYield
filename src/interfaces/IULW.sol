5593006724fa044a4e732f39245632db849a61ba
saurabh <saurabhyadav42399@gmail.com>
2025-08-25 01:47:36 +0530

    interface for ulw

---
+++ src/interfaces/IULW.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+interface IULW {
+    function getCalldata(address asset, uint256 amount, address receiver, uint8 operation, uint8 protocol)
+        external
+        view
+        returns (bytes memory data);
+}

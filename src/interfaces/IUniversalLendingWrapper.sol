36621bb666881740877539c53ce01571785c6724
saurabh <saurabhyadav42399@gmail.com>
2025-08-28 19:33:18 +0530

    added support for batching interface

--- src/interfaces/IUniversalLendingWrapper.sol
+++ src/interfaces/IUniversalLendingWrapper.sol
@@ -6,4 +6,21 @@ interface IUniversalLendingWrapper {
         external
         view
         returns (bytes memory data);
+    function getBatchSupplyBorrow(
+        address supplyAsset,
+        uint256 supplyAmount,
+        address borrowAsset,
+        uint256 borrowAmount,
+        address receiver,
+        uint8 protocol
+    ) external view returns (bytes memory supplyData, bytes memory borrowData);
+
+    function getBatchRepayWithdraw(
+        address repayAsset,
+        uint256 repayAmount,
+        address withdrawAsset,
+        uint256 withdrawAmount,
+        address receiver,
+        uint8 protocol
+    ) external view returns (bytes memory repayData, bytes memory withdrawData);
 }

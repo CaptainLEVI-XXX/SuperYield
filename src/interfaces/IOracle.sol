a52531e66e71cca69b41f7a94366b15ba864eb0c
saurabh <saurabhyadav42399@gmail.com>
2025-08-29 00:04:13 +0530

    added interface for oracle

--- src/interfaces/IOracle.sol
+++ src/interfaces/IOracle.sol
@@ -5,3 +5,24 @@ pragma solidity ^0.8.13;
 interface IOracle {
     function getAssetPrice(address asset) external view returns (uint256);
 }
+
+interface IUniswapV3Pool {
+    function observe(uint32[] calldata secondsAgos)
+        external
+        view
+        returns (int56[] memory tickCumulatives, uint160[] memory secondsPerLiquidityCumulativeX128s);
+    function token0() external view returns (address);
+    function token1() external view returns (address);
+}
+
+interface IChainlinkOracle {
+    function latestRoundData()
+        external
+        view
+        returns (uint80 roundId, int256 price, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+    function decimals() external view returns (uint8);
+}
+
+interface ICustomOracle {
+    function getPrice(address base, address quote) external view returns (uint256);
+}

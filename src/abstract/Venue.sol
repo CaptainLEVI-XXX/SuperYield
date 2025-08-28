7ba54d54b0addcbd8240b61874d9baebd2b150b3
saurabh <saurabhyadav42399@gmail.com>
2025-08-28 05:58:10 +0530

    addded venue support

--- src/abstract/Venue.sol
+++ src/abstract/Venue.sol
@@ -1,22 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-abstract contract Venue{
-
+abstract contract Venue {
     struct VenueInfo {
         address router;
         bool active;
         uint8 id;
     }
     // venue info
+
     mapping(bytes32 => VenueInfo) public venues;
 
-    function registerVenue(bytes32 venueId, address router,uint8 identifier) public virtual {
+    function registerVenue(bytes32 venueId, address router, uint8 identifier) public virtual {
         venues[venueId] = VenueInfo({router: router, active: true, id: identifier});
     }
 
     function setVenueStatus(bytes32 venueId, bool status) external virtual {
         venues[venueId].active = status;
     }
-    
 }

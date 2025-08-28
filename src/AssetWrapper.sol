a62f510132fc34dbda18f583bac5da4b9758112b
saurabh <saurabhyadav42399@gmail.com>
2025-08-28 05:54:16 +0530

    updated asset wrapper

--- src/AssetWrapper.sol
+++ src/AssetWrapper.sol
@@ -24,16 +24,10 @@ contract AssetVaultWrapper is Admin2Step, DexHelper {
         asset = vault.asset();
         incomingAsset = incomingAsset_;
 
-        // approve stETH to vault for deposits
-        asset.safeApprove(vault_, type(uint256).max);
-
         _setAdmin(owner_);
 
-        // Whitelist routes
-        whitelistRoute("1INCH-V6-A", 0x111111125421cA6dc452d289314280a0f8842A65);
-        whitelistRoute("PARASWAP-V6-A", 0x6A000F20005980200259B80c5102003040001068);
-        whitelistRoute("ZEROX-V4-A", 0xDef1C0ded9bec7F1a1670819833240f027b25EfF);
-        whitelistRoute("KYBER-AGGREGATOR-A", 0x6131B5fae19EA4f9D964eAc0408E4408b66337b5);
+        // approve stETH to vault for deposits
+        asset.safeApprove(vault_, type(uint256).max);
     }
 
     function whitelistRoute(string memory name, address _router) public virtual override onlyAdmin returns (bytes32) {
@@ -50,38 +44,27 @@ contract AssetVaultWrapper is Admin2Step, DexHelper {
         return super.updateRouteStatus(identifier, status);
     }
 
-    function deposit(bytes32 route_, bytes calldata swapCalldata_, address receiver_, uint256 amount_)
+    function deposit(DexSwapCalldata memory swapCalldata_, address receiver_, uint256 amount_)
         external
         returns (uint256 shares)
     {
-        uint256 balanceBefore_ = asset.balanceOf(address(this));
+        uint256 balanceBefore = asset.balanceOf(address(this));
 
         incomingAsset.safeTransferFrom(msg.sender, address(this), amount_);
 
-        performSwap(DexSwapCalldata({swapCalldata: swapCalldata_, identifier: route_}));
+        incomingAsset.safeApprove(getDexRouter(swapCalldata_.identifier), amount_);
 
-        uint256 depositAmount_ = asset.balanceOf(address(this)) - balanceBefore_ - 1;
+        performSwap(swapCalldata_);
+
+        uint256 balanceAfter = asset.balanceOf(address(this));
+
+        uint256 depositAmount_ = balanceAfter - balanceBefore;
 
         // deposit output into vault for msg.sender as receiver
         shares = vault.deposit(depositAmount_, receiver_);
     }
 
-    function mint(bytes32 route_, bytes calldata swapCalldata_, address receiver_, uint256 amount_)
-        external
-        returns (uint256 assets)
-    {
-        uint256 balanceBefore_ = asset.balanceOf(address(this));
-
-        incomingAsset.safeTransferFrom(msg.sender, address(this), amount_);
-
-        performSwap(DexSwapCalldata({swapCalldata: swapCalldata_, identifier: route_}));
-
-        uint256 depositAmount_ = asset.balanceOf(address(this)) - balanceBefore_ - 1;
-        // deposit output into vault for msg.sender as receiver
-        assets = vault.mint(depositAmount_, receiver_);
-    }
-
-    function withdraw(bytes32 route_, uint256 amount_, bytes calldata swapCalldata_, address receiver_)
+    function withdraw(DexSwapCalldata memory swapCalldata_, uint256 amount_, address receiver_)
         external
         returns (uint256 tokenAmount)
     {
@@ -95,33 +78,9 @@ contract AssetVaultWrapper is Admin2Step, DexHelper {
         if (vaultWithdrawnAmount < amount_ - 1) AssetVaultWrapper__UnexpectedWithdrawAmount.selector.revertWith();
 
         // approve stETH
-        asset.safeApprove(routeInfo().routes[route_].router, vaultWithdrawnAmount);
+        asset.safeApprove(getDexRouter(swapCalldata_.identifier), vaultWithdrawnAmount);
 
-        performSwap(DexSwapCalldata({swapCalldata: swapCalldata_, identifier: route_}));
-
-        tokenAmount = incomingAsset.balanceOf(address(this));
-
-        // transfer eth to receiver (usually msg.sender)
-        incomingAsset.safeTransfer(receiver_, tokenAmount);
-    }
-
-    function redeem(bytes32 route_, uint256 shares, bytes calldata swapCalldata_, address receiver_)
-        external
-        returns (uint256 tokenAmount)
-    {
-        uint256 balanceBefore = asset.balanceOf(address(this));
-
-        vault.redeem(shares, address(this), msg.sender);
-
-        uint256 vaultWithdrawnAmount = asset.balanceOf(address(this)) - balanceBefore;
-
-        // -1 to account for potential rounding errors
-        if (vaultWithdrawnAmount < shares - 1) AssetVaultWrapper__UnexpectedWithdrawAmount.selector.revertWith();
-
-        // approve stETH
-        asset.safeApprove(routeInfo().routes[route_].router, vaultWithdrawnAmount);
-
-        performSwap(DexSwapCalldata({swapCalldata: swapCalldata_, identifier: route_}));
+        performSwap(swapCalldata_);
 
         tokenAmount = incomingAsset.balanceOf(address(this));
 
